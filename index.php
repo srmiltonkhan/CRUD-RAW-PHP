5350b5ea342e6ae0a0a8d37d253b0bc140889ee4
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2022-04-07 12:51:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Md. Milton Khan">
     <title>CRUD Operation Row PHP</title>
     <link rel="stylesheet" href="style.css">
 </head>
